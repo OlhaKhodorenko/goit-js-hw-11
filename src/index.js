ea0c934e18152e9f8315cfec10db4bac9e21e6a3
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-05-20 16:58:23 +0300

    structure creation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './sass/main.scss';
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix, { Notify } from 'notiflix';
+
+
+const form = document.querySelector('#search-form');
+const galleryContainer = document.querySelector('.gallery');
